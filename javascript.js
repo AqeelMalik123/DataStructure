c50c155f057931b0736c16d1dbde20cc97a4c5fe
unknown <maqeel191@gmail.com>
2024-09-01 23:37:12 -0700

    searchFunctionality

--- javascript.js
+++ javascript.js
@@ -1,8 +1,19 @@
 
-let data=[10,20,30,40,50,60]
-let position=3
-for(let i=3; i<data.length-1;i++){
-    data[i]=data[i+1]
-    data.length=data.length-1
+// let data=[10,20,30,40,50,60]
+// let position=3
+// for(let i=3; i<data.length-1;i++){
+//     data[i]=data[i+1]
+//     data.length=data.length-1
+// }
+// console.log(data)
+
+// data search structure algorithm
+let data=[30,40,60,70,50,80]
+let searchNumber=40;
+for(let i=0; i<data.length; i++){
+    if(data[i]==searchNumber){
+        alert(data[i])
+    }else {
+        return "No Match Found"
+    }
 }
-console.log(data)
